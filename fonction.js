99d42958d44fbb1acc8bbc45b97190ea865343c1
Arthur Estrems <arthurjeu7@gmail.com>
2022-03-06 22:54:04 +0100

    Exercice 1

---
+++ fonction.js
@@ -0,0 +1,7 @@
+function additionner(nombre1, nombre2){
+    return parseInt(nombre1) + parseInt(nombre2);
+}
+
+console.log(additionner(2, 7));
+console.log(additionner(5, 7));
+console.log(additionner(8, 7));
